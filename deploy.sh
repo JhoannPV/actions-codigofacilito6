23e48d2de6e196588fcf957898104ccaad910299
JhoannPV <91164583+JhoannPV@users.noreply.github.com>
2025-06-18 18:37:04 -0500

    fix: update deployment workflow and script for branch-specific deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
+BRANCH=$1
+
+if ["$BRANCH" = "main"]; then
+    DEPLOY_PATH="/home/azureuser/actions-codigofacilito6"
+else
+    DEPLOY_PATH="/home/azureuser/stage"
+fi
+
+
 ssh -o StrictHostKeyChecking=no azureuser@48.217.49.228 <<HTML
 
-cd /home/azureuser/actions-codigofacilito6
+cd $DEPLOY_PATH
 
-git pull --rebase origin main
+git pull --rebase origin $BRANCH
 
 HTML
